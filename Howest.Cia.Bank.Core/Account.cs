c00d65cfa0485001ec9741c9f6fb06acb18cd6cd
Mattijs Vantorre <mattijsvantorre@me.com>
2022-02-16 18:52:26 +0100

    Initial commit

---
+++ Howest.Cia.Bank.Core/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Howest.Cia.Bank.Core
+{
+    public class Account
+    {
+        public decimal Balance { get; private set; }
+        
+        public Account()
+        {
+            Balance = 500;
+        }
+
+        public Account(decimal initialAmount)
+        {
+            Balance = initialAmount;
+        }
+
+        public decimal Deposit(decimal amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+    }
+}
